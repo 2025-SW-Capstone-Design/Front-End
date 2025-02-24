3be77e603f9be2577b1379cae35f0dfe92d9d502
zzangjyj0818 <jyj010818@naver.com>
2025-02-24 20:57:08 +0900

    ⚙️ Setting: Recoil 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,19 @@
 import { Suspense } from 'react';
 import { RouterProvider } from 'react-router-dom';
+import { QueryClientProvider } from '@tanstack/react-query';
+import { RecoilRoot } from 'recoil';
 
 import router from './Router';
-import { QueryClientProvider } from '@tanstack/react-query';
 import { queryClient } from './QueryClient';
 
 const App = () => {
   return (
     <QueryClientProvider client={queryClient}>
-      <Suspense fallback={<div>Loading...</div>}>
-        <RouterProvider router={router} />
-      </Suspense>
+      <RecoilRoot>
+        <Suspense fallback={<div>Loading...</div>}>
+          <RouterProvider router={router} />
+        </Suspense>
+      </RecoilRoot>
     </QueryClientProvider>
   );
 };
