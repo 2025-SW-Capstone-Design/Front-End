f4bb425222d60444c9d43892486f6d88c685b555
zzangjyj0818 <jyj010818@naver.com>
2025-03-04 18:42:51 +0900

    ⚙️ Setting: 전역 스타일 설정

--- src/App.tsx
+++ src/App.tsx
@@ -5,14 +5,21 @@ import { RecoilRoot } from 'recoil';
 
 import router from './Router';
 import { queryClient } from './QueryClient';
+import theme from './styles/theme';
+import { Global, ThemeProvider } from '@emotion/react';
+import globalStyles from './styles/globalStyle';
+import './App.css';
 
 const App = () => {
   return (
     <QueryClientProvider client={queryClient}>
       <RecoilRoot>
-        <Suspense fallback={<div>Loading...</div>}>
-          <RouterProvider router={router} />
-        </Suspense>
+        <ThemeProvider theme={theme}>
+          <Suspense fallback={<div>Loading...</div>}>
+            <Global styles={globalStyles} />
+            <RouterProvider router={router} />
+          </Suspense>
+        </ThemeProvider>
       </RecoilRoot>
     </QueryClientProvider>
   );
---
+++ src/styles/theme.ts
@@ -0,0 +1,339 @@
+const theme = {
+  colors: {
+    gray: {
+      10: '#F8F9FA',
+      20: '#F2F5F8',
+      30: '#E9EFF4',
+      40: '#DAE4EC',
+      50: '#C4CDD4',
+      60: '#AEB6BD',
+      70: '#83898E',
+      80: '#575B5E',
+      90: '#2C2E2F',
+    },
+    primary: {
+      10: '#EEF0FF',
+      20: '#D2D7FE',
+      30: '#9CA8FF',
+      40: '#697AFC',
+      50: '#5462CA',
+      60: '#4A55B0',
+      70: '#3F4997',
+      80: '#353D7E',
+      90: '#2A3165',
+      100: '#20254C',
+    },
+    red: {
+      10: '#FFD1D6',
+      20: '#FFB3BA',
+      30: '#FF7684',
+      40: '#FF6776',
+      50: '#EB5F6D',
+      60: '#CC525E',
+      70: '#993E47 ',
+      80: '#66292F',
+      90: '#331518',
+    },
+    purple: {
+      10: '#F1D0FF',
+      20: '#E7ACFF',
+      30: '#DF95FF',
+      40: '#DC89FF',
+      50: '#C67BE5',
+      60: '#B06ECC',
+      70: '#9A60B2',
+      80: '#845299',
+      90: '#583766',
+    },
+    yellow: {
+      10: '#FFFEE9',
+      20: '#FFFDD2',
+      30: '#FFFBB1',
+      40: '#FFF98F',
+      50: '#E5E081',
+      60: '#CCC772',
+      70: '#B2AE64',
+      80: '#999556',
+      90: '#666439',
+    },
+    green: {
+      10: '#DAFEF3',
+      20: '#B4FDE8',
+      30: '#7CFCD6',
+      40: '#44FAC5',
+      50: '#3DE1B1',
+      60: '#36C89E',
+      70: '#30AF8A',
+      80: '#299676',
+      90: '#1B644F',
+    },
+    system_red: {
+      10: '#FFD8D7',
+      20: '#FFB1AF',
+      30: '#FF8A86',
+      40: '#FF3C36',
+      50: '#661816',
+      60: '#330C0B',
+    },
+    system_blue: {
+      10: '#D3E0FF',
+      20: '#7CA1FF',
+      30: '#3A72FF',
+      40: '#2462FF',
+      50: '#1945B2',
+      60: '#0E2766',
+    },
+  },
+  text: {
+    display_bold: {
+      fontSize: '64px',
+      lineHeight: '125%',
+      fontWeight: 700,
+    },
+    display_medium: {
+      fontSize: '64px',
+      lineHeight: '125%',
+      fontWeight: 500,
+    },
+    display_light: {
+      fontSize: '64px',
+      lineHeight: '125%',
+      fontWeight: 300,
+    },
+    heading1_extra_bold: {
+      fontSize: '56px',
+      lineHeight: '125%',
+      fontWeight: '700',
+    },
+    heading1_bold: {
+      fontSize: '56px',
+      lineHeight: '125%',
+      fontWeight: '500',
+    },
+    heading1_medium: {
+      fontSize: '56px',
+      lineHeight: '125%',
+      fontWeight: '300',
+    },
+    heading2_extra_bold: {
+      fontSize: '48px',
+      lineHeight: '125%',
+      fontWeight: '700',
+    },
+    heading2_bold: {
+      fontSize: '48px',
+      lineHeight: '125%',
+      fontWeight: '500',
+    },
+    heading2_medium: {
+      fontSize: '48px',
+      lineHeight: '125%',
+      fontWeight: '300',
+    },
+    titleL_bold: {
+      fontSize: '32px',
+      lineHeight: '125%',
+      fontWeight: '700',
+    },
+    titleL_medium: {
+      fontSize: '32px',
+      lineHeight: '125%',
+      fontWeight: '500',
+    },
+    titleL_light: {
+      fontSize: '32px',
+      lineHeight: '125%',
+      fontWeight: '300',
+    },
+    titleM_bold: {
+      fontSize: '28px',
+      lineHeight: '150%',
+      fontWeight: '700',
+    },
+    titleM_medium: {
+      fontSize: '28px',
+      lineHeight: '150%',
+      fontWeight: '500',
+    },
+    titleM_light: {
+      fontSize: '28px',
+      lineHeight: '150%',
+      fontWeight: '300',
+    },
+    titleS_bold: {
+      fontSize: '24px',
+      lineHeight: '150%',
+      fontWeight: '700',
+    },
+    titleS_medium: {
+      fontSize: '24px',
+      lineHeight: '150%',
+      fontWeight: '500',
+    },
+    titleS_light: {
+      fontSize: '24px',
+      lineHeight: '150%',
+      fontWeight: '300',
+    },
+    bodyXL_bold: {
+      fontSize: '22px',
+      lineHeight: '150%',
+      fontWeight: '700',
+    },
+    bodyXL_medium: {
+      fontSize: '22px',
+      lineHeight: '150%',
+      fontWeight: '500',
+    },
+    bodyXL_light: {
+      fontSize: '22px',
+      lineHeight: '150%',
+      fontWeight: '300',
+    },
+
+    bodyL_bold: {
+      fontSize: '18px',
+      lineHeight: '150%',
+      fontWeight: '700',
+    },
+    bodyL_medium: {
+      fontSize: '18px',
+      lineHeight: '150%',
+      fontWeight: '500',
+    },
+    bodyL_light: {
+      fontSize: '18px',
+      lineHeight: '150%',
+      fontWeight: '300',
+    },
+
+    bodyM_bold: {
+      fontSize: '16px',
+      lineHeight: '150%',
+      fontWeight: '700',
+    },
+    bodyM_medium: {
+      fontSize: '16px',
+      lineHeight: '150%',
+      fontWeight: '500',
+    },
+    bodyM_light: {
+      fontSize: '16px',
+      lineHeight: '150%',
+      fontWeight: '300',
+    },
+
+    bodyS_bold: {
+      fontSize: '14px',
+      lineHeight: '150%',
+      fontWeight: '700',
+    },
+    bodyS_medium: {
+      fontSize: '14px',
+      lineHeight: '150%',
+      fontWeight: '500',
+    },
+    bodyS_light: {
+      fontSize: '14px',
+      lineHeight: '150%',
+      fontWeight: '300',
+    },
+    labelL_bold: {
+      fontSize: '14px',
+      lineHeight: '150%',
+      fontWeight: '700',
+    },
+    labelL_medium: {
+      fontSize: '14px',
+      lineHeight: '150%',
+      fontWeight: '500',
+    },
+    labelL_light: {
+      fontSize: '14px',
+      lineHeight: '150%',
+      fontWeight: '300',
+    },
+    labelM_bold: {
+      fontSize: '12px',
+      lineHeight: '150%',
+      fontWeight: '700',
+    },
+    labelM_medium: {
+      fontSize: '12px',
+      lineHeight: '150%',
+      fontWeight: '500',
+    },
+    labelM_light: {
+      fontSize: '12px',
+      lineHeight: '150%',
+      fontWeight: '300',
+    },
+    labeS_bold: {
+      fontSize: '10px',
+      lineHeight: '150%',
+      fontWeight: '700',
+    },
+    labeS_medium: {
+      fontSize: '10px',
+      lineHeight: '150%',
+      fontWeight: '500',
+    },
+    labelS_light: {
+      fontSize: '10px',
+      lineHeight: '150%',
+      fontWeight: '300',
+    },
+    detail_bold: {
+      fontSize: '10px',
+      lineHeight: '150%',
+      fontWeight: '700',
+    },
+    detail_medium: {
+      fontSize: '10px',
+      lineHeight: '150%',
+      fontWeight: '500',
+    },
+    detail_light: {
+      fontSize: '10px',
+      lineHeight: '150%',
+      fontWeight: '300',
+    },
+  },
+  shadow: {
+    normal: {
+      boxShadow:
+        '0px 2.906px 5.811px 0px rgba(0, 0, 0, 0.12), 0px 0px 2.906px 0px rgba(0, 0, 0, 0.08), 0px 0px 2.906px 0px rgba(0, 0, 0, 0.08)',
+    },
+    emphasize: {
+      boxShadow:
+        '0px 5.811px 23.246px 0px rgba(0, 0, 0, 0.12), 0px 2.906px 11.623px 0px rgba(0, 0, 0, 0.08), 0px 0px 2.906px 0px rgba(0, 0, 0, 0.08)',
+    },
+    strong: {
+      boxShadow:
+        '0px 17.434px 34.869px 0px rgba(0, 0, 0, 0.12), 0px 11.623px 23.246px 0px rgba(0, 0, 0, 0.08), 0px 0px 11.623px 0px rgba(0, 0, 0, 0.08)',
+    },
+    floating: {
+      boxShadow:
+        '0px 7.000000476837158px 23.000001907348633px rgba(0, 0, 0, 0.05)',
+    },
+    heavy: {
+      boxShadow: '0px 0px 23.24595069885254px rgba(0, 0, 0, 0.08)',
+    },
+  },
+  background: {
+    normal: {
+      background: '#FFF',
+    },
+    alternative: {
+      background: ' #ECF0F6',
+    },
+    gradient: {
+      background: 'linear-gradient(180deg, #F3F2F2 0%, #D7DCE4 100%)',
+    },
+    opacity: {
+      background: 'rgba(241, 243, 245, 0.60)',
+    },
+  },
+};
+
+export default theme;
