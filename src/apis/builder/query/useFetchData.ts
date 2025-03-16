42200816fd302bbafb5b31c68e31e1a004a182bb
zzangjyj0818 <jyj010818@naver.com>
2025-03-17 00:08:20 +0900

    🔧 Chore: 기존 ApiBuilder 삭제

--- src/apis/builder/query/useFetchData.ts
+++
@@ -1,9 +0,0 @@
-import { useQuery } from '@tanstack/react-query';
-import { apiBuilder } from '../apiBuilder';
-
-export const useFetchData = <T>(key: string, url: string) => {
-  return useQuery<T>({
-    queryKey: [key],
-    queryFn: () => apiBuilder<T>('get', url),
-  });
-};
