00d29c194394bfb4820a6cd670675003db5116cd
chacha <chayeonggeon@gmail.com>
2025-04-09 21:31:41 +0900

    Feat: 로그인페이지 라우터 추가

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,11 +1,16 @@
 import React from 'react';
 import { createBrowserRouter } from 'react-router-dom';
 import MainPage from './pages/MainPage';
+import LoginPage from './pages/LoginPage';
 
 const router = createBrowserRouter([
   {
     path: '/',
     element: <MainPage />,
+  },
+  {
+    path: '/login',
+    element: <LoginPage />,
   },
 ]);
 
