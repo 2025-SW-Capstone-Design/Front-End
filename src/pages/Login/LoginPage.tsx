5c86241c0d2237603268197f17592523103c5238
chacha <chayeonggeon@gmail.com>
2025-04-10 16:16:11 +0900

    Feat: 로그인 기능 구현

--- src/pages/Login/LoginPage.tsx
+++ src/pages/Login/LoginPage.tsx
@@ -3,9 +3,26 @@ import * as S from './LoginPage.styles';
 import logo from '../../assets/logo_white.svg';
 import keyboard_arrow_down from '../../assets/keyboard_arrow_down.svg';
 import github from '../../assets/github.svg';
+import { useNavigate } from 'react-router-dom';
 
 // NOTE 슬로건, url은 추후에 변경될 예정
+// NOTE 서비스 소개, 기능안내, HOME 버튼은 페이지 추가 후 연결 예정
 function LoginPage() {
+  const BaseURL = process.env.BASE_URL ?? '';
+  const navigate = useNavigate();
+
+  const handleBackButtonClick = () => {
+    navigate(-1);
+  };
+
+  const handleLoginClick = () => {
+    window.location.href = BaseURL + '/oauth2/authorization/github';
+  };
+
+  const handleGithubButtonClick = () => {
+    window.location.href = 'https://github.com/';
+  };
+
   return (
     <S.Container>
       <S.IntroContainer>
@@ -18,7 +35,7 @@ function LoginPage() {
         </S.IntroInfo>
       </S.IntroContainer>
       <S.LoginContainer>
-        <S.BackButton>
+        <S.BackButton onClick={handleBackButtonClick}>
           <img src={keyboard_arrow_down} alt="" />
           돌아가기
         </S.BackButton>
@@ -35,10 +52,12 @@ function LoginPage() {
               github
             </S.LoginContent>
             <S.LoginFooter>
-              <S.LoginButton>로그인</S.LoginButton>
+              <S.LoginButton onClick={handleLoginClick}>로그인</S.LoginButton>
               <S.LoginFooterText>
                 <p>깃허브 계정이 없으신가요? </p>
-                <button>깃허브 바로가기</button>
+                <button onClick={handleGithubButtonClick}>
+                  깃허브 바로가기
+                </button>
               </S.LoginFooterText>
             </S.LoginFooter>
           </S.LoginSection>
