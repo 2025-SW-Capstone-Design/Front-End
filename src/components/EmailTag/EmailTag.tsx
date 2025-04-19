86dd5a1f2b477b1364021cce765d09a4d7ce5be8
chacha <chayeonggeon@gmail.com>
2025-04-20 00:03:54 +0900

    Feat: 이메일 태그 타입 정의

--- src/components/EmailTag/EmailTag.tsx
+++ src/components/EmailTag/EmailTag.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import * as S from './EmailTag.styles';
 import { ReactComponent as X } from '../../assets/icon/X_small.svg';
+import type { EmailTagProps } from './EmailTag.types';
 
-function EmailTag() {
+function EmailTag({ email, onRemove }: EmailTagProps) {
   return (
     <S.EmailTagWrapper>
-      <S.Email>email@gmail.com</S.Email>
-      <S.RemoveButton>
+      <S.Email>{email}</S.Email>
+      <S.RemoveButton onClick={onRemove}>
         <X />
       </S.RemoveButton>
     </S.EmailTagWrapper>
