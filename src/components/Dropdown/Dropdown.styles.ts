a9db6e8953c1cb1ed06ccad4634682c9d44a231d
chacha <chayeonggeon@gmail.com>
2025-05-02 02:47:42 +0900

    Feat: 드롭다운 디자인 구현

---
+++ src/components/Dropdown/Dropdown.styles.ts
@@ -0,0 +1,80 @@
+import styled from '@emotion/styled';
+import type { DropdownStylesProps } from './Dropdown.types';
+
+export const Container = styled.div<DropdownStylesProps>`
+  position: relative;
+  width: ${({ width }) => width || '168px'};
+  height: 44px;
+`;
+
+export const SelectBox = styled.div<{
+  isOpen: boolean;
+  dropdownType: 'default' | 'primary';
+}>`
+  padding: 10px 24px;
+  color: ${({ theme, dropdownType }) =>
+    dropdownType === 'primary'
+      ? theme.colors.primary[40]
+      : theme.colors.gray[80]};
+  outline: 2px solid 
+  ${({ theme, dropdownType }) =>
+    dropdownType === 'primary'
+      ? theme.colors.primary[30]
+      : theme.colors.gray[60]};
+  outline-offset: -2px;
+  border-radius: 8px;
+  background-color: white;
+  cursor: pointer;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+
+  ${({ theme }) => theme.text.bodyM_medium};
+  ${({ isOpen, theme, dropdownType }) =>
+    isOpen &&
+    `outline: 2px solid ${
+      dropdownType === 'primary'
+        ? theme.colors.primary[40]
+        : theme.colors.gray[80]
+    };
+  `}
+
+  svg{
+  width: 24px;
+  height: 24px;
+  fill: ${({ theme, dropdownType }) =>
+    dropdownType === 'primary'
+      ? theme.colors.primary[40]
+      : theme.colors.gray[80]};
+`;
+
+export const OptionList = styled.ul<{ dropdownType: 'default' | 'primary' }>`
+  position: absolute;
+  top: calc(100% - 1px);
+  left: 0;
+  width: 100%;
+  outline: 2px solid
+    ${({ theme, dropdownType }) =>
+      dropdownType === 'primary'
+        ? theme.colors.primary[30]
+        : theme.colors.gray[60]};
+  outline-offset: -2px;
+  border-radius: 8px;
+  background-color: white;
+  z-index: 10;
+`;
+
+export const OptionItem = styled.li<{ isSelected: boolean }>`
+  padding: 10px 24px;
+  height: 40px;
+  color: ${({ theme }) => theme.colors.gray[90]};
+  ${({ theme }) => theme.text.bodyM_medium};
+  border-radius: 8px;
+  border-bottom: 2px solid ${({ theme }) => theme.colors.gray[20]};
+  cursor: pointer;
+
+  background-color: white;
+  &:hover {
+    background-color: ${({ theme }) => theme.colors.gray[30]};
+  }
+`;
