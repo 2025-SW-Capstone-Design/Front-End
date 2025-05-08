6c112949feb30f935737b03f21e673bcdb4dd7c4
zzangjyj0818 <jyj010818@naver.com>
2025-05-08 19:05:10 +0900

    Refactor: Api Builder의 React-Query 수정

--- src/Router.tsx
+++ src/Router.tsx
@@ -6,6 +6,8 @@ import IntroPage from './pages/Intro/IntroPage';
 import TokenProccesor from './utils/Authorization/TokenProccesor';
 import ProtectedLayout from './layouts/ProtectedLayout';
 import TeamPage from './pages/Team/TeamPage';
+import CalendarPage from './pages/Calendar/CalendarPage';
+import TaskCreatePage from './pages/Task/TaskCreatePage';
 
 const router = createBrowserRouter([
   { path: '/login', element: <LoginPage /> },
@@ -17,6 +19,11 @@ const router = createBrowserRouter([
     children: [
       { index: true, element: <MainPage /> },
       { path: 'team/:teamId', element: <TeamPage /> },
+      { path: 'team/:teamId/kant', element: <CalendarPage /> },
+      {
+        path: 'team/:teamId/project/:projectId/task/create',
+        element: <TaskCreatePage />,
+      },
     ],
   },
 ]);
