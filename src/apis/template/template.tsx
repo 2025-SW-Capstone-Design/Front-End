6d981da08f1681062e92832eeee1040f8ac5075d
zzangjyj0818 <jyj010818@naver.com>
2025-05-12 02:19:37 +0900

    Fix: 인스턴스 생성 시, 타입이 잘못 들어가있는 오류 해결

--- src/apis/template/template.tsx
+++ src/apis/template/template.tsx
@@ -6,6 +6,8 @@ import type {
 
 const END_POINT = {
   TEMPLATE: (teamId: number) => `/api/v1/teams/${teamId}/issue-templates`,
+  TEMPLATES: (teamId: number, projectId: number) =>
+    `/api/v1/teams/${teamId}/issue-templates/projects/${projectId}`,
   TEMPLATE_DETAIL: (teamId: number, templateId: number) =>
     `/api/v1/teams/${teamId}/issue-templates/${templateId}`,
 };
@@ -17,7 +19,7 @@ const createTemplate = (teamId: number) => {
 };
 
 const getTemplateDetail = (teamId: number, templateId: number) => {
-  return ApiBuilder.create<IssueTemplateResponse, void>(
+  return ApiBuilder.create<void, IssueTemplateResponse>(
     END_POINT.TEMPLATE_DETAIL(teamId, templateId),
   ).setMethod('GET');
 };
@@ -34,9 +36,9 @@ const updateTemplate = (teamId: number, templateId: number) => {
   ).setMethod('PATCH');
 };
 
-const getTemplates = (teamId: number) => {
-  return ApiBuilder.create<IssueTemplateResponse[], void>(
-    END_POINT.TEMPLATE(teamId),
+const getTemplates = (teamId: number, projectId: number) => {
+  return ApiBuilder.create<void, IssueTemplateResponse[]>(
+    END_POINT.TEMPLATES(teamId, projectId),
   ).setMethod('GET');
 };
 
