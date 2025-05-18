3f797c0fb19d95c623065b4952503b3746e879d9
zzangjyj0818 <jyj010818@naver.com>
2025-05-19 05:22:23 +0900

    Feat: MainPage 텍스트 추가

--- src/pages/Main/MainPage.styles.ts
+++ src/pages/Main/MainPage.styles.ts
@@ -1,3 +1,18 @@
 import styled from '@emotion/styled';
 
-export {};
+const MainContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+
+  width: 100%;
+  height: 100vh;
+
+  font-size: 30px;
+  font-weight: 700;
+
+  align-items: center;
+  justify-content: center;
+
+  color: ${(props) => props.theme.colors.gray[70]};
+`;
+export { MainContainer };
